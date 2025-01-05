88b3c191802847ab07420c664ef0743181a5656e
singhMukund <mk789699@gmail.com>
2025-01-05 22:06:52 +0530

    Loading Screen

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,19 +1,16 @@
 // Game.ts
-import { Application, Assets, Container, Loader, Sprite, Ticker } from 'pixi.js';
-import { Howl } from 'howler';
-import { CommonConfig } from '../Common/CommonConfig';
-import { CommonEvents } from '@/Common/CommonEvents';
-import { BaseGame } from './State/Basegame';
-import { GameConfig } from './GameConfiguration/GameConfig';
-import { FreeGame } from './State/Freegame';
-import { StateManagement } from './State/StateManagement';
-import SoundManager from './Sound/SoundManager';
-// import manifest from "../../public/manifest.json";
+import { Application, Assets, Container } from "pixi.js";
+import { CommonConfig } from "../Common/CommonConfig";
+import { CommonEvents } from "@/Common/CommonEvents";
+import { GameConfig } from "./GameConfiguration/GameConfig";
+import { StateManagement } from "./State/StateManagement";
+import { LoadingScreen } from "./Loading/LoadingScreen";
 export class Game {
   protected static _the: Game;
   public app: Application;
   private gameContainer!: Container;
   private isLocaltesting: boolean = false;
+  private loadingScreen!: LoadingScreen;
 
   static get the(): Game {
     if (!Game._the) {
@@ -33,7 +30,7 @@ export class Game {
 
   async init(): Promise<void> {
     await this.app.init();
-    const pixiContainer = document.getElementById('pixi-container');
+    const pixiContainer = document.getElementById("pixi-container");
     if (pixiContainer) {
       pixiContainer.appendChild(this.app.canvas);
     }
@@ -45,44 +42,34 @@ export class Game {
     window.onresize = this.resize.bind(this);
   }
 
+  private initLoadingObj(): void {
+    this.loadingScreen = new LoadingScreen();
+    this.app.stage.addChild(this.loadingScreen);
+  }
+
   private async loadImages() {
+    this.initLoadingObj();
     await Assets.init({ manifest: "./manifest.json" });
-    await Assets.loadBundle(["background-image", "ReelFrame-Component", "static-symbol", "static-button", "win_animation", "Feature-Assets"]);
+    await Assets.loadBundle([
+      "background-image",
+      "ReelFrame-Component",
+      "static-symbol",
+      "static-button",
+      "win_animation",
+      "Feature-Assets",
+    ],(data)=>{
+      console.log(data);
+      this.loadingScreen.loadingAnimation(data);
+    });
 
-    // const sound = new Howl({
-    //   src: ['audio.wav', 'audio.m4a', 'audio.mp3'],
-    // });
-
-    // // Define your audio files
-    // const audioFiles = {
-    //   background: ['./audio/BG_Music.wav', './audio/BG_Music.m4a'],
-    // };
-
-    // const audioConfig = {
-    //   background: { src: ['./audio/BG_Music.wav', './audio/BG_Music.wav'], volume: 0.5, loop: true },
-    // };
-
-    // const sounds = Object.fromEntries(
-    //   Object.entries(audioConfig).map(([key, config]) => [
-    //     key,
-    //     new Howl(config),
-    //   ])
-    // );
-
-    // Assets.events.on('progress', (progress: number) => {
-    //   const percentage = Math.round(progress * 100);
-    //   this.updateLoadingScreen(percentage);
-    // });
     this.onLoadComplete();
   }
 
   isIOS(): boolean {
-    const audio = document.createElement('audio');
-    return audio.canPlayType('audio/ogg; codecs="vorbis"') === '';
-    return false
+    const audio = document.createElement("audio");
+    return audio.canPlayType('audio/ogg; codecs="vorbis"') === "";
+    return false;
   }
-
-
 
   private loadAssetsAndInitialize() {
     this.loadImages();
@@ -91,20 +78,15 @@ export class Game {
     new GameConfig();
   }
 
-
-
   private onLoadComplete() {
+    this.loadingScreen.visible = false;
+    this.loadingScreen.alpha = 0;
     this.app.stage.addChild(new StateManagement());
     // new SoundManager();
   }
-
-
-
 
   resize() {
     this.app.stage.emit("RESIZE_THE_APP");
     this.app.renderer.resize(window.innerWidth, window.innerHeight);
   }
-
-
 }
